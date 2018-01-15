36bea347c53f61fe5d9fbdef40a218e166a719b6
Matjaz Prtenjak <matjaz@matjazev.net>
2018-01-15 09:46:43 +0100

    Add project files.

---
+++ MNet.Log4NetExtensions/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNet.Log4NetExtensions
+{
+    public class Class1
+    {
+    }
+}
