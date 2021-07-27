f636528b30d5d7107ac18225d8b98841ff2f1670
Matjaz Prtenjak <matjaz@matjazev.net>
2021-07-27 12:15:29 +0200

    Add one-line converter

---
+++ MNet.Log4NetExtensions/OneLineMessagePatternConvertor.cs
@@ -0,0 +1,17 @@
+﻿using log4net.Util;
+using System.IO;
+
+namespace MNet.Log4NetExtensions
+{
+  public class OneLineMessagePatternConvertor : PatternConverter
+  {
+    protected override void Convert(TextWriter writer, object state)
+    {
+      var loggingEvent = state as log4net.Core.LoggingEvent;
+      if (loggingEvent == null)
+        return;
+
+      writer.Write(loggingEvent.RenderedMessage.Replace("\r", " ").Replace("\n", " "));
+    }
+  }
+}
---
+++ MNet.Log4NetExtensions/OneLineMessagePatternLayout.cs
@@ -0,0 +1,18 @@
+﻿using log4net.Layout;
+using log4net.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNet.Log4NetExtensions
+{
+  public class OneLineMessagePatternLayout : PatternLayout
+  {
+    public OneLineMessagePatternLayout()
+    {
+      AddConverter(new ConverterInfo { Name = "one_line_message", Type = typeof(OneLineMessagePatternConvertor) });
+    }
+  }
+}
