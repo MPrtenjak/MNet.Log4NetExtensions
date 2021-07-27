8004115d311f23812217159c7a5587239d169e21
Matjaz Prtenjak <matjaz@matjazev.net>
2021-07-27 14:59:06 +0200

    New version

--- MNet.Log4NetExtensions/Properties/AssemblyInfo.cs
+++ MNet.Log4NetExtensions/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MNet.Log4NetExtensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Log4Net Extension for accumulation logs for some period of time and process logging only if enough messages are received")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mnet")]
 [assembly: AssemblyProduct("MNet.Log4NetExtensions")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
